c5192862f08f69c5c6605aba76d51a25836e9a82
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-05-07 18:50:51 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,40 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void addCage(bool light) {
+  Cage *newCage = new Cage;
+  newCage->light = light;
+  newCage->next = nullptr;
+  newCage->prev = nullptr;
+  
+  if (first == nullptr) {
+    first = newCage;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Cage *last = first->prev;
+    last->next = newCage;
+    newCage->prev = last;
+    newCage->next = first;
+    first->prev = newCage;
+  }
+}
+
+int getLength() {
+  if (first == nullptr)
+    return 0;
+
+  int length = 1;
+  Cage *current = first->next;
+
+  while (current != first) {
+    length++;
+    current = current->next;
+  }
+
+  return length;
+}
+
+int getOpCount() {
+  return countOp;
+}
